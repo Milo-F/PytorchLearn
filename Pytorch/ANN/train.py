8c2a4336a6704fafde514646dd8cf18bffb2162e
Milo-F <milo.fox168@gmail.com>
2020-09-23 21:20:19 +0800

    modify

--- Pytorch/ANN/train.py
+++ Pytorch/ANN/train.py
@@ -6,12 +6,12 @@ input_dim, hidden_dim, output_dim = 3, 140, 3
 lr = 1e-3
 train_rate, valid_rate, test_rate = 0.7, 0.1, 0.2
 model = ANNModel(input_dim, hidden_dim, output_dim)
-loss_fn = torch.nn.MSELoss("sum")
+loss_fn = torch.nn.MSELoss(reduction = "sum")
 optimizer = torch.optim.Adam(model.parameters(), lr)
 
 # 创建随机数据
-test_in = torch.randn(512, input_dim)
-test_out = torch.randn(512, output_dim)
+test_in = torch.randn(128, input_dim)
+test_out = torch.randn(128, output_dim)
 
 for times in range(500):
     pred_out = model.forward(test_in)
